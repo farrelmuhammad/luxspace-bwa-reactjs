f24e13527fefee5109733c8c2d3a30bbce2fc531
Farrel Muhammad <farrelf11@gmail.com>
2023-02-11 15:00:35 +0700

    Create hooks useScrollToTop

---
+++ .history/src/helpers/hooks/useScrollToTop_20230211145712.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useLayoutEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+export default function useScrollToTop() {
+  const navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+    
+  }, [navigate.length])
+}
